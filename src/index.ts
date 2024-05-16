a1a34047bccb4069939ab281499cb0f4cbf03ab7
ymio <ymio243@gmail.com>
2024-05-16 22:39:35 +0800

    chore: add https serve

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 import path from 'path';
+import https from 'https';
+import fs from 'fs';
 
 const app: Express = express();
-const port: number = 3000;
+const port: number = 443; // HTTPS 默认端口
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, '..', 'server.key')),
+  cert: fs.readFileSync(path.join(__dirname, '..', 'server.crt'))
+};
 
 // 设置静态文件目录
 app.use(express.static(path.join(__dirname, '..', 'public')));
@@ -13,6 +20,6 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 // 启动服务器
-app.listen(port, () => {
-  console.log(`服务器运行在 http://localhost:${port}/`);
+https.createServer(options, app).listen(port, () => {
+  console.log(`服务器运行在 https://localhost:${port}/`);
 });
